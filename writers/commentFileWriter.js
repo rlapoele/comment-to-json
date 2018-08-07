e19710c0f5d0ed44f88ef20efb047a0950bb7e39
Renaud Lapoele (rlapoele) <rlapoele@cisco.com>
2018-08-07 23:30:06 +0200

    Release 1.0.5 - CHANGELOG.md release headings made smaller. - Bug Fixes:   - certain combinations of source/destination file paths were not handled properly.   - if destination path included non-existent directory(ies), an error was thrown.   - depending on options passed to script, some processing statuses were incorrectly rendered.

--- writers/commentFileWriter.js
+++ writers/commentFileWriter.js
@@ -24,25 +24,43 @@ function commentFileWriter(comments, commentsSourceFilePath, commentsDestination
    * Actual/real comment file writer.
    */
   function writeCommentToFile() {
+  
+    /**
+     * Create missing path directories.
+     * @param {string} filePath
+     * @returns {boolean}
+     */
+    function ensureDirectoryExistence(filePath) {
+      const dirname = path.dirname(filePath);
+      if (fs.existsSync(dirname)) {
+        return true;
+      }
+      ensureDirectoryExistence(dirname);
+      fs.mkdirSync(dirname);
+    }
+  
     const unfilteredCommentCount = comments.length;
     const filteredComments = keepAllComments ? comments : comments.filter(comment => !!comment.annotations.length);
+  
     if (filteredComments.length > 0) {
+      ensureDirectoryExistence(commentsDestinationFilePath);
       fs.writeFile(
         commentsDestinationFilePath,
         JSON.stringify(filteredComments, null, 2),
         function (err) {
-          if (err) {
+          if(err) {
             console.error(err);
           }
           else {
-            const commentLabel = `${!keepAllComments?'annotated':''} comment${filteredComments.length>1?'s':''}`;
-            console.log(`"${chalk.green(`${basename(commentsSourceFilePath)}${path.extname(commentsSourceFilePath)}`)}": ${chalk.yellow(chalk.bold(filteredComments.length))} ${chalk.yellow(commentLabel)} parsed (out of ${!keepAllComments?unfilteredCommentCount:''}) and saved in "${chalk.blue(commentsDestinationFilePath)}"`);
+            const commentLabel = `${!keepAllComments?'annotated ':''}comment${filteredComments.length>1?'s':''}`;
+            const outOfLabel = !keepAllComments ? `(out of ${unfilteredCommentCount}) `: '';
+            console.log(`"${chalk.green(`${basename(commentsSourceFilePath)}${path.extname(commentsSourceFilePath)}`)}": ${chalk.yellow(chalk.bold(filteredComments.length))} ${chalk.yellow(commentLabel)} parsed ${outOfLabel}and saved in "${chalk.blue(commentsDestinationFilePath)}"`);
           }
         }
       );
     }
     else {
-      console.log(`"${chalk.red(`${basename(commentsSourceFilePath)}${path.extname(commentsSourceFilePath)}`)}": ${chalk.red(chalk.bold('0'))} ${chalk.red('comments')} parsed.`);
+      console.log(`"${chalk.red(`${basename(commentsSourceFilePath)}${path.extname(commentsSourceFilePath)}`)}": ${chalk.red(chalk.bold('0'))} ${chalk.red('annotated comments')} parsed.`);
     }
   }
   
